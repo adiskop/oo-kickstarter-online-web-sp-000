a27014c2abcd24289b40dd2bbc72a5cb2f426596
Adi Skop <adiskop@gmail.com>
2020-07-21 11:19:15 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
-attr_accessor :name, :Project
+attr_accessor :name, :backed_projects
 
 def initialize(name)
   @name=name
   @backed_projects=[]
 end
 
-def back_project(Project)
-  @backed_project<<Project
+def back_project(project)
+  @backed_project<<project
 end
 
 
